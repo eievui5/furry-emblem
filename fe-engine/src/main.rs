efd8aca476448be2d6f983aac83bacf9c423a861
Evie <14899090+eievui5@users.noreply.github.com>
2023-08-25 18:49:58 -0400

    Add tilemap handling

--- fe-engine/src/main.rs
+++ fe-engine/src/main.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 use bevy::window::WindowMode;
+use bevy_ecs_tilemap::prelude::*;
 use fe_engine::cursor;
 use fe_engine::module;
 use fe_engine::ppcanvas::PixelPerfectCanvas;
@@ -12,7 +13,6 @@ const WINDOW_SIZE: UVec2 = UVec2::new(240, 160);
 fn set_icon(window: &PistonWindow, icon: DynamicImage) -> anyhow::Result<()> {
 	// TODO: embed the default icon into the binary (like fe-editor) so that it is always available.
 	// TODO: allow the icon to be overwritten by projects.
-	// `piston_window` very inconsiderately has a module named `image` so we need this root syntax.
 	let width = icon.width();
 	let height = icon.height();
 	let icon = icon.to_rgba8().into_vec();
@@ -63,8 +63,9 @@ fn main() {
 				}),
 			PixelPerfectCanvas::<{ WINDOW_SIZE.x }, { WINDOW_SIZE.y }>,
 			InputManagerPlugin::<cursor::UiAction>::default(),
+			TilemapPlugin,
 		))
-		.add_systems(Startup, (cursor::spawn, spawn_unit))
+		.add_systems(Startup, (cursor::spawn, spawn_unit, startup))
 		.add_systems(Update, (cursor::movement, cursor::rotate))
 		.add_systems(Update, fullscreen)
 		.run();
@@ -73,11 +74,14 @@ fn main() {
 fn fullscreen(mut windows: Query<&mut Window>, keys: Res<Input<KeyCode>>) {
 	// TODO: Use an event for this.
 	if keys.just_pressed(KeyCode::F11) {
+		// Borderless is th eonly fullscreen mode that works on Wayland.
+		const FULLSCREEN: WindowMode = WindowMode::BorderlessFullscreen;
+
 		let mut window = windows.single_mut();
-		window.mode = if window.mode == WindowMode::Fullscreen {
+		window.mode = if window.mode == FULLSCREEN {
 			WindowMode::Windowed
 		} else {
-			WindowMode::Fullscreen
+			FULLSCREEN
 		};
 	}
 }
@@ -95,6 +99,56 @@ fn spawn_unit(
 	commands.spawn(SpriteSheetBundle {
 		texture_atlas,
 		sprite: TextureAtlasSprite::new(0),
+		..Default::default()
+	});
+}
+
+fn startup(
+	mut commands: Commands,
+	asset_server: Res<AssetServer>,
+	array_texture_loader: Res<ArrayTextureLoader>,
+) {
+	let texture_handle: Handle<Image> = asset_server.load("example-game/tileset.png");
+	let map_size = TilemapSize { x: 15, y: 11 };
+	let tilemap_entity = commands.spawn_empty().id();
+	let mut tile_storage = TileStorage::empty(map_size);
+
+	for x in 0..map_size.x {
+		for y in 0..map_size.y {
+			let tile_pos = TilePos { x, y };
+			let tile_entity = commands
+				.spawn(TileBundle {
+					position: tile_pos,
+					tilemap_id: TilemapId(tilemap_entity),
+					..Default::default()
+				})
+				.id();
+			tile_storage.set(&tile_pos, tile_entity);
+		}
+	}
+
+	let tile_size = TilemapTileSize { x: 16.0, y: 16.0 };
+	let grid_size = tile_size.into();
+	let map_type = TilemapType::default();
+	let mut transform = get_tilemap_center_transform(&map_size, &grid_size, &map_type, 0.0);
+	transform.translation.z -= 16.0;
+
+	commands.entity(tilemap_entity).insert(TilemapBundle {
+		grid_size,
+		map_type,
+		size: map_size,
+		storage: tile_storage,
+		texture: TilemapTexture::Single(texture_handle.clone()),
+		tile_size,
+		transform,
+		..Default::default()
+	});
+
+	// Add atlas to array texture loader so it's preprocessed before we need to use it.
+	// Only used when the atlas feature is off and we are using array textures.
+	array_texture_loader.add(TilemapArrayTexture {
+		texture: TilemapTexture::Single(texture_handle),
+		tile_size,
 		..Default::default()
 	});
 }
